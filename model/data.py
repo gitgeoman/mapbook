669b26822e9d6265363530310ba5b43077ae6db8
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2024-06-10 22:42:07 +0200

    wip: prepare data source

---
+++ model/data.py
@@ -0,0 +1,7 @@
+users: list = [
+
+    {"name": "Jul", "surname": "Gie", "posts": 1500, 'location': 'Tłuszcz_(miasto)'},
+    {"name": "Hub", "surname": "Sy", "posts": 123456, 'location': 'Warszawa'},
+    {"name": "Adr", "surname": "Do", "posts": 3, 'location': 'Ząbki'},
+    {"name": "Bar", "surname": "Wy", "posts": 666, 'location': 'Praga'}
+]
